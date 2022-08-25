953c7d2451b3de0ccd69143aa07e158db93f5f09
thufailnaufal99 <thufailnaufal99@gmail.com>
2022-08-25 13:27:46 +0700

    Add update task

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const server = express();
 const main = require("./scr/main");
 const DB = require("./scr/config/DB");
@@ -9,11 +10,21 @@ async function init() {
     await DB.connect();
     server.use(express.urlencoded({ extended: true }));
     server.use(express.json());
-    server.use("/images", express.static("images"))
+    server.use("/images", express.static("images"));
     server.use("/api/v1", main);
-    server.get('/', (req, res) => {
-      res.send("Selamat datang di Tickitz")
-    })
+    server.get("/", (req, res) => {
+      res.send("Selamat datang di Tickitz");
+    });
+    const whitelist = [`http://localhost:${process.env.APP_PORT}`];
+    const corsOptionsDelegate = function (req, callback) {
+      let corsOptions;
+      if (whitelist.indexOf(req.header("Origin")) !== -1) {
+        corsOptions = { origin: true };
+      } else {
+        corsOptions = { origin: false };
+      }
+      callback(null, corsOptions);
+    };
 
     server.listen(process.env.APP_PORT, () => {
       console.log("Sever ready");
